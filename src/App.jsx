36d1c44386807f03613145421e7b72e4e2586504
chunkytstudios <chunkythunderstudios@gmail.com>
2026-03-10 00:58:33 -0400

    Update App.jsx 2
    fixed the useref errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback, useRef } from 'react'
 import { weeks, quizQuestions, schoolData } from './data.js'
 import PhilChat from './PhilChat.jsx'
 import duckImg from '/phil.png'
